42fed6b7b8dc3ba9eb5479e08177880af6c74245
marina.puelker <marina.puelker@ort.org>
2026-07-30 16:29:51 -0300

    usuario

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
+import react
+ from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Dimensions, Text, View } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <text style={styles.greeting}>Bom dia, Marina.</text>
+        <input 
+        
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +20,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
   },
+  header: {
+    width: '100%',
+    height: Dimensions.get('window').height/6,
+    backgroundColor: '#BC72DE',
+    justifyContent: 'center',
+  }
 });
