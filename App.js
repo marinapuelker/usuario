1071c86364f532f09ec8eefab562dd1eb3a07709
giovanna.bem <gidbem2009@gmail.com>
2026-08-04 16:29:23 -0300

    editei

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import react
- from 'react';
+import react from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Dimensions, Text, View,} from 'react-native';
 import { TextInput } from 'react-native-web';
@@ -9,12 +8,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.greeting}>Bom dia, aluno!</Text>
     <View style={styles.boxInput}>
 
           <TextInput
             style={styles.input}
-            placeholder='Pesquisar...'
           
         
             />
@@ -37,21 +34,18 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     justifyContent: 'center',
   },
-  greeting:{
-    fontSize: 20,
-    color: '#FFF',
-    marginTop: 100,
-  },
   boxInput:{
     width: '90%',
-    marginTop: 20,
     marginLeft: 20,
+    marginTop: 70,
   },
   input: {
-    borderRadius: 3,
+    borderRadius: 8,
     borderColor: 'white',
     height: 30,
     backgroundColor: 'white',
+    marginTop: 100,
+    padding: 21,
   },
   //boxList:{
     //flex: 1,
