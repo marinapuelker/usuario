a5966ef112db53b378225488fe00b77ad30b98b7
marinapuelker <marina.puelker@ort.org.br>
2026-08-03 21:00:09 -0300

    Update greeting text and input field in App.js

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import react
  from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Dimensions, Text, View } from 'react-native';
+import { StyleSheet, Dimensions, Text, View,} from 'react-native';
+import { TextInput } from 'react-native-web';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <text style={styles.greeting}>Bom dia, Marina.</text>
-        <input 
+        <text style={styles.greeting}>Bom dia, aluno!</text>
+        <View style={styles.boxInput}>
+
+          <input
+            style={styles.input}
+            placeholder='Pesquisar...'
+          
         
-        />
+            />
+          </View>
+        <View style={styles.boxList}>
+        </View>
       </View>
     </View>
   );
@@ -27,6 +36,20 @@ const styles = StyleSheet.create({
     width: '100%',
     height: Dimensions.get('window').height/6,
     backgroundColor: '#BC72DE',
+    paddingHorizontal: 20,
     justifyContent: 'center',
-  }
+  },
+  greeting:{
+    fontSize: 20,
+    color: '#FFF',
+    marginTop: 20,
+  },
+  boxInput:{
+    width: '80%',
+  },
+  //boxList:{
+    //flex: 1,
+   // width: '100%',
+    //backgroundColor: 'red',
+  //},
 });
